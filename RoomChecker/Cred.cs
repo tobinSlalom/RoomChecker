5b9db861470b85cb3f5d345fd76217cab060dde2
Tobin Pomeroy <tobin.pomeroy@slalom.com>
2017-12-06 13:28:47 -0800

    initial commit

---
+++ RoomChecker/Cred.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RoomChecker
+{
+    public class Cred
+    {
+        internal static string e = "YOUR_EMAIL";
+        internal static string p = "YOUR_PASS";
+
+    }
+}
